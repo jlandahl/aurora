0d13268cd85413ca50db7629a1a017ac8f374757
John Landahl <john@landahl.org>
2025-04-20 18:26:44 -0700

    Yet another Bitwarden RPM path fix
    Still don't know what directory to use, but root seems to work.

--- files/scripts/download-bitwarden.sh
+++ files/scripts/download-bitwarden.sh
@@ -6,7 +6,7 @@ URL=$(curl -s https://api.github.com/repos/bitwarden/clients/releases | jq -r 'f
 
 if [ -n "$URL" ]; then
     echo "Downloading Bitwarden from $URL"
-    curl -sL -o /tmp/bitwarden-latest.rpm "$URL"
+    curl -sL -o /bitwarden-latest.rpm "$URL"
 else
     echo "--- Could not find Bitwarden RPM URL"
     exit 1
