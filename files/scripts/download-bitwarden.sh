4fc25246f17068dc284146ff641d56010d6405e8
John Landahl <john@landahl.org>
2025-04-20 17:51:49 -0700

    Added /tmp path for Bitwarden RPM

--- files/scripts/download-bitwarden.sh
+++ files/scripts/download-bitwarden.sh
@@ -5,7 +5,9 @@ set -euo pipefail
 URL=$(curl -s https://api.github.com/repos/bitwarden/clients/releases | jq -r 'first(.[] | .assets[]? | select(.browser_download_url | endswith(".rpm")) | .browser_download_url)')
 
 if [ -n "$URL" ]; then
-    curl -sL -o bitwarden-latest.rpm "$URL"
+    echo "Downloading Bitwarden from $URL"
+    curl -sL -o /tmp/bitwarden-latest.rpm "$URL"
 else
     echo "--- Could not find Bitwarden RPM URL"
+    exit 1
 fi
